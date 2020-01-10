5a4995b408a3c4c7069c0804a3318d212cfecef9
Daniel Zhang <daniel@rpia.ca>
2020-01-10 14:11:03 -0500

    SANTA-367 changed error handling logic

--- santa-fe/src/app/modules/trade/services/LiveDataProcessingService.ts
+++ santa-fe/src/app/modules/trade/services/LiveDataProcessingService.ts
@@ -54,7 +54,7 @@ export class LiveDataProcessingService {
         const newBESecurity:BESecurityDTO = serverReturn[eachKey][0].security;
         const newSecurity = this.dtoService.formSecurityCardObject(eachKey, newBESecurity, false);
         serverReturn[eachKey].forEach((eachPortfolio: BEPortfolioDTO) => {
-          if (eachPortfolio.quantity !== 0 && !eachPortfolio.security.isGovt && eachPortfolio.security.metrics) {
+          if (eachPortfolio.quantity !== 0 && !eachPortfolio.security.isGovt) {
             this.dtoService.appendPortfolioInfoToSecurityDTO(newSecurity, eachPortfolio, activeMetricType);
           } else {
             isValidFlag = false;
