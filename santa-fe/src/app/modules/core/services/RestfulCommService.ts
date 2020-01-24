0b4c8edf44e7fe9e67c26d804689e7f25fe85659
Daniel Zhang <daniel@rpia.ca>
2020-01-24 11:57:32 -0500

    pointing dev to the right BE

--- santa-fe/src/app/modules/core/services/RestfulCommService.ts
+++ santa-fe/src/app/modules/core/services/RestfulCommService.ts
@@ -7,8 +7,8 @@ import { APIUrlMap } from 'Core/constants/coreConstants.constant';
 @Injectable()
 export class RestfulCommService {
 
-  private endpoint = 'https://rpiadev01:1225';
-  // private endpoint = 'https://rpia-quant-dt:51225';
+  // private endpoint = 'https://rpiadev01:1225';
+  private endpoint = 'https://rpia-quant-dt:51225';
   //private endpoint = 'https://rpia-solutions:51225';
   //private endpoint = 'https://rpia-msmith-dt:51225';
   public apiMap = {
