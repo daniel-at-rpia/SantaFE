61789b6e117649611ac013ff8e6bd3bf123cb96c
Daniel Zhang <daniel@rpia.ca>
2020-03-04 17:59:11 -0500

    bump version and deploy dev

--- santa-fe/webpack.partial.js
+++ santa-fe/webpack.partial.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-      "VERSION": JSON.stringify("1.1.7")
+      "VERSION": JSON.stringify("1.1.8")
     })
   ]
 }
