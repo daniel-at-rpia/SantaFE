256436aba3db6adb2e7c2d9133c94214cdce8b73
Shirley <scao@rpia.ca>
2020-08-11 12:20:34 -0400

    added constants

---
+++ santa-fe/src/app/modules/core/constants/structureConstants.constants.ts
@@ -0,0 +1,242 @@
+import { PortfolioStructureDTO } from 'FEModels/frontend-models.interface';
+
+export const PortfolioBlock: Array<PortfolioStructureDTO> = [
+  {
+    data: {
+      portfolioName: 'DOF',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  },
+  {
+    data: {
+      portfolioName: 'SOF',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  },
+  {
+    data: {
+      portfolioName: 'STIP',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  },
+  {
+    data: {
+      portfolioName: 'FIP',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  },
+  {
+    data: {
+      portfolioName: 'CIP',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  },
+  {
+    data: {
+      portfolioName: 'AGB',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  },
+  {
+    data: {
+      portfolioName: 'BBB',
+      marketValue: {
+        data: {
+          metricName: 'Market Value',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      },
+      CS01: {
+        data: {
+          metricName: 'CS01',
+          currentValue: 0,
+          targetValue: 0, 
+          difference: 0.
+        },
+        state: {
+          isStencil: false,
+          isSelected: false,
+          isTargetExceeded: false
+        }
+      }
+    }, 
+    state: {
+      isEditing: false
+    }
+  }
+]
