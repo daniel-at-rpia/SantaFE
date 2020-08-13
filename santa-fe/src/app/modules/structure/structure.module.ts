eb9c3d6b63d87103551878cccf48561e9bf81203
Shirley <scao@rpia.ca>
2020-08-13 17:54:10 -0400

    #69 added reference to target bar component in structure module

--- santa-fe/src/app/modules/structure/structure.module.ts
+++ santa-fe/src/app/modules/structure/structure.module.ts
@@ -7,12 +7,14 @@ import { StructurePage } from 'Structure/structure.page';
 
 import { StructureMainPanel } from 'Structure/containers/structure-main-panel.container';
 import { StructureFund } from 'App/modules/structure/containers/structure-fund.container';
+import { TargetBar } from 'App/modules/structure/components/target-bar.component'
 
 @NgModule({
   declarations: [
     StructurePage,
     StructureMainPanel,
-    StructureFund
+    StructureFund, 
+    TargetBar
   ],
   imports: [
     // Angular framework modules
