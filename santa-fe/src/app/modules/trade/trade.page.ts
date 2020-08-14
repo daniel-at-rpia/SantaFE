c18507491ea371b0f7c025a21fe2c8cffacdc074
Shirley <scao@rpia.ca>
2020-08-14 15:14:31 -0400

    #68 set userInitial to trade page state from ngRX store

--- santa-fe/src/app/modules/trade/trade.page.ts
+++ santa-fe/src/app/modules/trade/trade.page.ts
@@ -21,7 +21,7 @@
     import { TradeState } from 'FEModels/frontend-page-states.interface';
     import { selectSelectedSecurityForAnalysis } from 'Trade/selectors/trade.selectors';
     import { CoreUserLoggedIn } from 'Core/actions/core.actions';
-    import { selectDislayAlertThumbnail } from 'Core/selectors/core.selectors';
+    import { selectDislayAlertThumbnail, ownerInitials } from 'Core/selectors/core.selectors';
     import { SecurityMapEntry } from 'FEModels/frontend-adhoc-packages.interface';
     import { CoreLoadSecurityMap } from 'Core/actions/core.actions';
   //
@@ -36,7 +36,8 @@ export class TradePage implements OnInit, OnDestroy {
   state: TradeState;
   subscriptions = {
     receiveSelectedSecuritySub: null,
-    displayAlertThumbnailSub: null
+    displayAlertThumbnailSub: null,
+    ownerInitialsSub: null
   };
 
   private initializePageState() {
@@ -69,6 +70,11 @@ export class TradePage implements OnInit, OnDestroy {
       select(selectDislayAlertThumbnail)
     ).subscribe((value) => {
       this.state.displayAlertThumbnail = !!value;
+    });
+    this.subscriptions.ownerInitialsSub = this.store$.pipe(
+      select(ownerInitials)
+    ).subscribe((value) => {
+      this.state.ownerInitial = value;
     });
   }
 
