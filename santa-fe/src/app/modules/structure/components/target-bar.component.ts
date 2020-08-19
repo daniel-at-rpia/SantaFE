e8de39154520d0cb3cffb2b8da89e4c9458d32d4
Shirley <scao@rpia.ca>
2020-08-19 11:35:37 -0400

    #69 moved all inputs and functions to target bar DTO

--- santa-fe/src/app/modules/structure/components/target-bar.component.ts
+++ santa-fe/src/app/modules/structure/components/target-bar.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
-
-import { PortfolioMetricValues } from 'Core/constants/structureConstants.constants';
+import { TargetBarDTO } from 'App/modules/core/models/frontend/frontend-models.interface';
 
 @Component({
   selector: 'target-bar',
@@ -10,47 +9,16 @@ import { PortfolioMetricValues } from 'Core/constants/structureConstants.constan
 })
 
 export class TargetBar implements OnInit {
-  @Input() targetMetric: string; 
-  @Input() currentValue: number;
-  @Input() targetValue: number;
-  @Input() selectedMetricValue: PortfolioMetricValues;
-  @Input() stencilState;
-  currentPercentage: string;
-  exceededPercentage: string;
-  displayedCurrentValue: string;
-  displayedTotalValue: string;
-  state = {
-   isInactiveMetric: false,
-   isStencil: this.stencilState
-  }
+  @Input() targetBar: TargetBarDTO;
   constructor() {}
-  ngOnInit() {
-    this.getDisplayedValues();
-    this.convertValuesForDisplay();
-    this.setInactiveMetric();
+
+  public ngOnInit() {
+   this.loadTargetBarConfiguration(this.targetBar);
   }
 
-private getDisplayedValues() {
-  if (this.currentValue > this.targetValue) {
-    const difference = this.currentValue - this.targetValue; 
-    this.currentPercentage = '100%';
-    this.exceededPercentage = this.currentValue / this.targetValue >= 2 ? '100%' : `${(difference / this.targetValue) * 100}%`
-    return;
-  }
-    this.currentPercentage = `${(this.currentValue / this.targetValue) * 100}%`;
-  }
-
-  private setInactiveMetric() {
-    this.state.isInactiveMetric = this.targetMetric !== this.selectedMetricValue ? true : false;
-  }
-
-  private convertValuesForDisplay() {
-   if (this.targetMetric === PortfolioMetricValues.CSO1) {
-    this.displayedCurrentValue= `${this.currentValue}K`;
-    this.displayedTotalValue= `${this.targetValue}K`;
-    return;
-  }
-   this.displayedCurrentValue = `${this.currentValue}`;
-   this.displayedTotalValue = `${this.targetValue}`;
+  private loadTargetBarConfiguration(targetBar: TargetBarDTO) {
+    this.targetBar.utility.getDisplayValues(targetBar);
+    this.targetBar.utility.setInactiveMetric(targetBar);
+    this.targetBar.utility.convertNumtoStr(targetBar);
   }
 }
--- santa-fe/src/app/modules/structure/containers/structure-fund.container.ts
+++ santa-fe/src/app/modules/structure/containers/structure-fund.container.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { PortfolioStructureDTO } from 'Core/models/frontend/frontend-models.interface';
-
 import {PortfolioMetricValues } from 'Core/constants/structureConstants.constants';
+import { DTOService } from 'Core/services/DTOService';
+import { TargetBarDTO } from 'FEModels/frontend-models.interface';
 
 
 @Component({
@@ -14,15 +14,53 @@ import {PortfolioMetricValues } from 'Core/constants/structureConstants.constant
 
 export class StructureFund implements OnInit {
   @Input() fund: PortfolioStructureDTO;
-  @Input() selectedMetricValue;
-
+  @Input() selectedMetricValue: PortfolioMetricValues;
+  targetBarCS01: TargetBarDTO;
+  targetBarLeverage: TargetBarDTO; 
   constants = {
     CS01: PortfolioMetricValues.CSO1,
-    Leverage: PortfolioMetricValues.Leverage
+    leverage: PortfolioMetricValues.Leverage
   }
 
+  constructor(private dtoService: DTOService) {}
 
-  constructor() {}
+  public ngOnInit() {
+    this.targetBarCS01 = this.createTargetBar(this.constants.CS01, this.fund.data.currentTotals.CS01, this.fund.data.targetTotals.CS01, this.selectedMetricValue);
+    this.targetBarLeverage = this.createTargetBar(this.constants.leverage, this.fund.data.currentTotals.leverage, this.fund.data.targetTotals.leverage, this.selectedMetricValue)
 
-  public ngOnInit() {}
+  }
+
+  private createTargetBar(constantValue: PortfolioMetricValues, currentValue: number, targetValue: number, selectedMetric: PortfolioMetricValues) {
+    const newTargetBar = this.dtoService.formTargetBarObject(constantValue, currentValue,targetValue, selectedMetric);
+    newTargetBar.utility.getDisplayValues = this.getDisplayedValues;
+    newTargetBar.utility.setInactiveMetric = this.setInactiveMetric;
+    newTargetBar.utility.convertNumtoStr = this.convertValuesForDisplay;
+    return newTargetBar
+  }
+
+  private getDisplayedValues(targetBar: TargetBarDTO) {
+    if (targetBar.data.currentValue > targetBar.data.targetValue) {
+      const difference = targetBar.data.currentValue - targetBar.data.targetValue;
+      targetBar.data.currentPercentage = '100%';
+      targetBar.data.exceededPercentage = targetBar.data.currentValue / targetBar.data.targetValue >= 2 ? '100%' : `${(difference / targetBar.data.targetValue) * 100}%`
+      return;
+    }
+    targetBar.data.currentPercentage = `${(targetBar.data.currentValue / targetBar.data.targetValue) * 100}%`;
+  }
+
+  private setInactiveMetric(targetBar: TargetBarDTO) {
+    targetBar.state.isInactiveMetric = targetBar.data.targetMetric !== targetBar.data.selectedMetricValue ? true : false;
+  }
+
+  private convertValuesForDisplay(targetBar: TargetBarDTO) {
+    if (targetBar.data.targetMetric === PortfolioMetricValues.CSO1) {
+      const formattedCurrentValue = `${targetBar.data.currentValue / 1000}K`;
+      const formattedTargetValue = `${targetBar.data.targetValue / 1000}K`;
+      targetBar.data.displayedCurrentValue = formattedCurrentValue;
+      targetBar.data.displayedTargetValue = formattedTargetValue;
+      return;
+    }
+    targetBar.data.displayedCurrentValue = `${targetBar.data.currentValue}`;
+    targetBar.data.displayedTargetValue = `${targetBar.data.targetValue}`;
+  }
 }
