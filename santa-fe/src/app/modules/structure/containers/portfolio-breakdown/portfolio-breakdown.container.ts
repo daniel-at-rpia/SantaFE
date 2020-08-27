f328cac62a2e47cd9564ab5ba48dd2e40cd2b8d7
Shirley <scao@rpia.ca>
2020-08-27 18:36:51 -0400

    #81 set constants and added input value of selectedMetricValue to portfolio breakdown

--- santa-fe/src/app/modules/structure/containers/portfolio-breakdown/portfolio-breakdown.container.ts
+++ santa-fe/src/app/modules/structure/containers/portfolio-breakdown/portfolio-breakdown.container.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 
 import { PortfolioBreakdownDTO } from 'FEModels/frontend-models.interface';
-
+import { PortfolioMetricValues } from 'Core/constants/structureConstants.constants';
 @Component({
   selector: 'portfolio-breakdown',
   templateUrl: './portfolio-breakdown.container.html',
@@ -11,6 +11,11 @@ import { PortfolioBreakdownDTO } from 'FEModels/frontend-models.interface';
 
 export class PortfolioBreakdown {
   @Input() breakdownData: PortfolioBreakdownDTO;
+  @Input() selectedMetricValue: PortfolioMetricValues;
+  constants = {
+    cs01: PortfolioMetricValues.cs01,
+    creditLeverage: PortfolioMetricValues.creditLeverage
+  }
   constructor() { }
 
 }
