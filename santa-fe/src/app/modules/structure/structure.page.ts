444e34844ea0bc1480517233933858404ba6ea12
Shirley <scao@rpia.ca>
2020-09-19 17:09:24 -0400

    #129-FetchBICsHierarchy added functionality to created nested BICs hierarchy in structure page

--- santa-fe/src/app/modules/structure/structure.page.ts
+++ santa-fe/src/app/modules/structure/structure.page.ts
@@ -21,6 +21,8 @@
     import { StructureState } from 'FEModels/frontend-page-states.interface';
     import { StructureStoreResetEvent } from 'Structure/actions/structure.actions';
     import { STRUCTURE_EDIT_MODAL_ID } from 'Core/constants/structureConstants.constants';
+    import { BICsHierarchyAllDataBlock, BICsHierarchyBlock } from '../core/models/frontend/frontend-blocks.interface';
+    import { BEBICsHierarchyBlock } from 'Core/models/backend/backend-models.interface';
   //
 
 @Component({
@@ -37,19 +39,77 @@ export class StructurePage implements OnInit, OnDestroy {
     editModalId: STRUCTURE_EDIT_MODAL_ID
   };
 
-  private initializePageState() {}
+  private initializePageState(): StructureState {
+    const state: StructureState = {
+      BICSData: {
+        formattedBICsHierarchy: {
+          children: [],
+        },
+        tierCounter: 1
+      },
+      fetchResult: {
+        fetchBICsHierarchyFailed: false,
+        fetchBICsHierarchyError: '',
+        fetchBICsHierarchy: null
+      }
+    }
+    return state;
+  }
 
   constructor(
     private store$: Store<any>,
     private dtoService: DTOService,
     private utilityService: UtilityService,
     private restfulCommService: RestfulCommService
-  ) {}
+  ) {
+    this.state = this.initializePageState();
+  }
 
   public ngOnInit() {
+    this.state = this.initializePageState();
     this.store$.dispatch(new StructureStoreResetEvent);
+    this.fetchBICsHierarchy();
   }
 
   public ngOnDestroy() {
+  }
+
+  private updateBICsFetch(receivedData: boolean, data: BEBICsHierarchyBlock, message: string = '') {
+    this.state.fetchResult.fetchBICsHierarchyFailed = !receivedData;
+    this.state.fetchResult.fetchBICsHierarchy = data;
+    this.state.fetchResult.fetchBICsHierarchyError = message;
+  }
+  private fetchBICsHierarchy() {
+    this.restfulCommService.callAPI(this.restfulCommService.apiMap.getBICsHierarchy, {req: 'GET'}).pipe(
+      first(),
+      tap((serverReturn: BEBICsHierarchyBlock) => {
+       if (!!serverReturn) {
+         this.updateBICsFetch(true, serverReturn);
+         if (!!this.state.fetchResult.fetchBICsHierarchy) {
+          this.iterateBICsData(this.state.fetchResult.fetchBICsHierarchy, this.state.BICSData.formattedBICsHierarchy, this.state.BICSData.tierCounter);
+         }
+       }
+      }),
+      catchError(err => {
+        this.updateBICsFetch(false, null, err);
+        this.restfulCommService.logError('Cannot retrieve BICs hierarchy data');
+        return of('error');
+      })
+    ).subscribe()
+  }
+
+  private iterateBICsData(data: BEBICsHierarchyBlock, parent: BICsHierarchyAllDataBlock | BICsHierarchyBlock, counter: number) {
+    if (!data) return;
+    for (let category in data) {
+      if (!!data[category] && !!category) {
+        const BICsData: BICsHierarchyBlock = {
+          name: category,
+          tier: counter,
+          children: []
+        }
+        parent.children.push(BICsData);
+        this.iterateBICsData(data[category], BICsData, BICsData.tier + 1);
+      }
+    }
   }
 }
