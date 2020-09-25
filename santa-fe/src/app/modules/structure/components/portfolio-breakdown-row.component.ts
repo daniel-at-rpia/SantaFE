e003c872b5610f31bca836427564c7f22f7457f0
Shirley <scao@rpia.ca>
2020-09-25 11:44:07 -0400

    #82-BICSDiveIn-p1 renamed category content list item to portfolio breakdown row

---
+++ santa-fe/src/app/modules/structure/components/portfolio-breakdown-row.component.ts
@@ -0,0 +1,39 @@
+import {Component, Input, Output, OnInit, ViewEncapsulation, EventEmitter} from '@angular/core';
+import { StructurePortfolioBreakdownRowDTO } from 'Core/models/frontend/frontend-models.interface';
+import { Store, select } from '@ngrx/store';
+import { selectMetricLevel } from 'Structure/selectors/structure.selectors';
+import { PortfolioMetricValues } from 'App/modules/core/constants/structureConstants.constants';
+
+
+@Component({
+  selector: 'portfolio-breakdown-row',
+  templateUrl: './portfolio-breakdown-row.component.html',
+  styleUrls: ['./portfolio-breakdown-row.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+
+export class PortfolioBreakdownRow implements OnInit {
+  @Input() breakdownRow: StructurePortfolioBreakdownRowDTO;
+  @Output() breakdownRowDiveIn = new EventEmitter<StructurePortfolioBreakdownRowDTO>();
+  subscriptions = {
+    selectedMetricLevelSub: null
+  }
+  constructor(private store$: Store<any>) {}
+  public ngOnInit() {
+    this.subscriptions.selectedMetricLevelSub = this.store$.pipe(
+      select(selectMetricLevel)
+    ).subscribe((value) => {
+      if (!!value) {
+        this.breakdownRow.data.state.isDisplayCs01 = value === PortfolioMetricValues.cs01;
+        if (this.breakdownRow.data.children) {
+          this.breakdownRow.data.children.data.displayCategoryList = this.breakdownRow.data.state.isDisplayCs01 ?  this.breakdownRow.data.children.data.rawCs01CategoryList :  this.breakdownRow.data.children.data.rawLeverageCategoryList;
+        }
+      }
+    });
+  };
+
+  public onClickDiveIn(row: StructurePortfolioBreakdownRowDTO) {
+    console.log(row, 'breakdown row');
+    !!this.breakdownRowDiveIn && this.breakdownRowDiveIn.emit(row);
+  }
+}
