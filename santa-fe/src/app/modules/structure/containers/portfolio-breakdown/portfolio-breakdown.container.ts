de7114903907f317f687a06f137aac39f78956c0
Shirley <scao@rpia.ca>
2020-09-30 11:21:42 -0400

    #82-BICsDiveIn-p1 updated popover logic to work with switch and minor fixes

--- santa-fe/src/app/modules/structure/containers/portfolio-breakdown/portfolio-breakdown.container.ts
+++ santa-fe/src/app/modules/structure/containers/portfolio-breakdown/portfolio-breakdown.container.ts
@@ -3,14 +3,14 @@ import { of, Subscription } from 'rxjs';
 import { catchError, first, tap} from 'rxjs/operators';
 import { Store, select } from '@ngrx/store';
 
-import { PortfolioBreakdownDTO, StructurePortfolioBreakdownRowDTO } from 'FEModels/frontend-models.interface';
+import { PortfolioBreakdownDTO, StructurePopoverDTO, StructurePortfolioBreakdownRowDTO } from 'FEModels/frontend-models.interface';
 import { PortfolioMetricValues, STRUCTURE_EDIT_MODAL_ID } from 'Core/constants/structureConstants.constants';
 import { ModalService } from 'Form/services/ModalService';
 import { UtilityService } from 'Core/services/UtilityService';
 import { selectUserInitials } from 'Core/selectors/core.selectors';
 import { BICsDataProcessingService } from 'Structure/services/BICsDataProcessingService';
 import { DTOService } from 'Core/services/DTOService';
-
+import { PortfolioBreakdownCategoryBlock } from 'Core/models/frontend/frontend-blocks.interface'; 
 @Component({
   selector: 'portfolio-breakdown',
   templateUrl: './portfolio-breakdown.container.html',
@@ -64,10 +64,13 @@ export class PortfolioBreakdown implements OnInit, OnChanges, OnDestroy {
     this.breakdownData.data.displayCategoryList = this.breakdownData.state.isDisplayingCs01 ? this.breakdownData.data.rawCs01CategoryList : this.breakdownData.data.rawLeverageCategoryList;
     if (this.dataIsReady) {
       this.calculateAlignmentRating();
-     if (!!this.breakdownData.data.popover) {
+     if (!!this.breakdownData.data.popover && !!this.breakdownData.data.popover.state.isActive) {
+      const previousMetricData = this.utilityService.deepCopy(this.breakdownData.data.popover.data.mainRow.data.children);
       const popoverCategory = this.breakdownData.data.popover.data.mainRow.data.category; 
       const popoverRow = this.breakdownData.state.isDisplayingCs01 ? this.breakdownData.data.rawCs01CategoryList.find(row => row.data.category === popoverCategory) : this.breakdownData.data.rawLeverageCategoryList.find(row => row.data.category === popoverCategory);
       this.updatePopoverData(popoverRow);
+      this.breakdownData.data.popover.data.mainRow.data.children = previousMetricData;
+      this.switchPopoverValues(this.breakdownData.data.popover.data.mainRow.data);
      }
       const flipStencil = this.removeStencil.bind(this);
       setTimeout(() => {
@@ -114,10 +117,37 @@ export class PortfolioBreakdown implements OnInit, OnChanges, OnDestroy {
 
 public updatePopoverData(breakdownRow: StructurePortfolioBreakdownRowDTO)
  {
+  if (!!this.breakdownData.data.selectedCategory) {
+    if (breakdownRow.data.category !== this.breakdownData.data.selectedCategory) {
+      const previousRowCategory = this.breakdownData.data.selectedCategory;
+      this.breakdownData.data.selectedCategory = breakdownRow.data.category; 
+      const rowList = this.breakdownData.state.isDisplayingCs01 ? this.breakdownData.data.rawCs01CategoryList : this.breakdownData.data.rawLeverageCategoryList; 
+      const previousRow = rowList.find(row => row.data.category === previousRowCategory);
+      previousRow.state.isSelected = false;
+    }
+  } else {
+    this.breakdownData.data.selectedCategory = breakdownRow.data.category;
+  }
   const subBicsLevel = this.bicsDataProcessingService.formSubLevelBreakdown(breakdownRow, this.breakdownData.state.isDisplayingCs01);
   breakdownRow.data.children = subBicsLevel;
   this.breakdownData.data.popover = this.dtoService.formStructurePopoverObject(breakdownRow, this.breakdownData.state.isDisplayingCs01);
   this.breakdownData.data.popover.data.mainRow.state.isSelected = true;
   this.breakdownData.data.popover.state.isActive = true;
  }
+
+  public switchPopoverValues(block: PortfolioBreakdownCategoryBlock) {
+    if (!block.children) return;
+    const currentMetricList =  this.breakdownData.state.isDisplayingCs01 ? block.children.data.rawCs01CategoryList : block.children.data.rawLeverageCategoryList;
+    const oppositeMetricList = this.breakdownData.state.isDisplayingCs01 ? block.children.data.rawLeverageCategoryList : block.children.data.rawCs01CategoryList;
+    block.children.data.displayCategoryList = currentMetricList;
+    block.children.data.displayCategoryList.forEach(row => {
+      const selectedValue = oppositeMetricList.find(previousRow => previousRow.data.category === row.data.category);
+      row.state.isSelected = !!selectedValue.data.children && selectedValue.state.isSelected;
+      row.data.children = selectedValue.data.children;
+      if (!!row.data.children) {
+        row.data.children.data.displayCategoryList = this.breakdownData.state.isDisplayingCs01 ? row.data.children.data.rawCs01CategoryList : row.data.children.data.rawLeverageCategoryList;
+        this.switchPopoverValues(row.data);
+      }
+    })
+  }
 }
