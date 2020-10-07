03f20e9f752af6e7124bbd6fc8dd8a68814891c1
unknown <scao@rpia.ca>
2020-10-07 09:06:03 -0400

    #107-SetView removed selected category when  edit modal is triggered

--- santa-fe/src/app/modules/structure/containers/structure-set-target-panel/structure-set-target-panel.container.ts
+++ santa-fe/src/app/modules/structure/containers/structure-set-target-panel/structure-set-target-panel.container.ts
@@ -107,17 +107,21 @@ export class StructureSetTargetPanel implements OnInit, OnDestroy {
           if (!!this.state.targetBreakdown.data.popover) {
             this.state.targetBreakdown.data.popover.state.isActive = false;
           }
+          const selectedCategory = this.state.targetBreakdown.data.displayCategoryList.find(category => category.state.isSelected);
+          if (!!selectedCategory) {
+            selectedCategory.state.isSelected = false;
+          }
           if (this.state.targetBreakdown.data.displayCategoryList.length > 0) {
             this.state.targetBreakdown.data.displayCategoryList.forEach(row => {
               row.state.isEditingView = false;
-            })
-          }
+          })
         }
         this.state.targetBreakdownIsOverride = !!pack.isCreateNewOverride || pack.targetBreakdown.state.isOverrideVariant;
         this.state.targetBreakdownRawData = this.retrieveRawBreakdownDataForTargetBreakdown();
         this.state.activeMetric = pack.targetFund.data.cs01TargetBar.state.isInactiveMetric ? this.constants.metric.creditLeverage : this.constants.metric.cs01;
         this.loadEditRows();
         this.calculateAllocation();
+        }
       }
     })
     this.modalService.bindModalSaveCallback(STRUCTURE_EDIT_MODAL_ID, this.submitTargetChanges.bind(this));
