78f10521a243143e6caacd2b8d310b0db378a38d
Daniel Zhang <danielzhangshibo@gmail.com>
2021-02-03 17:32:48 -0500

    #359 IndexedDB base in

--- santa-fe/src/app/modules/core/services/GlobalWorkflowIOService.ts
+++ santa-fe/src/app/modules/core/services/GlobalWorkflowIOService.ts
@@ -9,12 +9,20 @@ import { GlobalWorkflowTypes } from 'Core/constants/coreConstants.constant';
 @Injectable()
 
 export class GlobalWorkflowIOService {
+  // given that storing workflow state is the only application of indexedDB in Santa at the moment, there is no need to over-engineer the indexedDB layer in santa, just put it in IOService for now
+  private INDEXEDDB_VERSION = 1;
+  private INDEXEDDB_WORKFLOW_DATABASE_NAME = 'GlobalWorkflow';
+
   private temporaryStore: Map<string, DTOs.GlobalWorkflowStateDTO> = new Map();
+  private workflowIndexedDBAPI: IDBDatabase;
 
   constructor(
     private utilityService: UtilityService,
     private dtoService: DTOService
-  ){}
+  ){
+    const openRequest = window.indexedDB.open(this.INDEXEDDB_WORKFLOW_DATABASE_NAME, this.INDEXEDDB_VERSION);
+    this.initiateIndexedDBRequestHandler(openRequest);
+  }
 
   public storeState(targetState: DTOs.GlobalWorkflowStateDTO) {
     const writableCopy = this.utilityService.deepCopy(targetState);
@@ -64,4 +72,30 @@ export class GlobalWorkflowIOService {
       return null;
     }
   }
+
+  private initiateIndexedDBRequestHandler(openRequest: IDBOpenDBRequest) {
+    openRequest.onerror = (errorEvent) => {
+      // do something if necessary
+    }
+
+    openRequest.onsuccess = (successEvent) => {
+      // do something if necessary
+    }
+
+    openRequest.onupgradeneeded = (newVersionDetectedEvent) => {
+      // reconstruct the database upon version change
+      this.workflowIndexedDBAPI = openRequest.result;
+      switch(newVersionDetectedEvent.oldVersion) {
+        case 0:
+          // version 0 means that the client had no database
+          // perform initialization
+          const objectStore = this.workflowIndexedDBAPI.createObjectStore("workflow", { keyPath: "id" });
+          break;
+        default:
+          window.indexedDB.deleteDatabase(this.INDEXEDDB_WORKFLOW_DATABASE_NAME);
+          break;
+      }
+    }
+  }
+
 }
