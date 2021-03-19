95d2a9fbbc0058588311806415a10cf98caa0196
Shirley <scao@rpia.ca>
2021-03-19 16:03:19 -0400

    #574 use timestamp for all alerts

--- santa-fe/src/app/modules/core/services/DTOService.ts
+++ santa-fe/src/app/modules/core/services/DTOService.ts
@@ -1596,7 +1596,7 @@ export class DTOService {
     rawData: BEModels.BEAlertDTO
   ): DTOs.AlertDTO {
     const parsedTitleList = rawData.keyWord.split('|');
-    const momentTime = !!rawData.trade ? moment(rawData.trade.eventTime): moment(rawData.timeStamp);
+    const momentTime = moment(rawData.timeStamp);
     const object: DTOs.AlertDTO = {
       data: {
         id: rawData.alertId,
