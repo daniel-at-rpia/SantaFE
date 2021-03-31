5fdd477cb5b6f96869b8d3d361b512227acbe585
Shirley <scao@rpia.ca>
2021-03-31 12:33:41 -0400

    #557 logically set an option's deepestLevel state

--- santa-fe/src/app/modules/core/models/frontend/frontend-blocks.interface.ts
+++ santa-fe/src/app/modules/core/models/frontend/frontend-blocks.interface.ts
@@ -112,6 +112,7 @@ export interface SecurityGroupPieChartColorSchemeBlock {
 export interface SecurityDefinitionFilterBlock {
   isSelected: boolean;  // means it is selected for filtering
   isFilteredOut: boolean;  // means it is hidden/disabled in the UI, sry about this naming failure, it's just not worth it to mass-rename it in the code
+  isDeepestLevel?: boolean; // for BICS only
   displayLabel: string;
   bicsLevel?: number;  // only exist for consolidated bics options
   shortKey: string;
