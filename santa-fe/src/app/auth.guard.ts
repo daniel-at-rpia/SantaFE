13263a02739546bd593750fc61f08f091e75797e
Daniel Zhang <danielzhangshibo@gmail.com>
2021-04-29 18:55:12 -0400

    #653 authGuard with api call

--- santa-fe/src/app/auth.guard.ts
+++ santa-fe/src/app/auth.guard.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable, Subscription, interval, of } from 'rxjs';
+import { tap, map, delay,
+      catchError,first, withLatestFrom, switchMap, filter } from 'rxjs/operators';
 
 import { RestfulCommService } from 'Core/services/RestfulCommService';
 
@@ -12,9 +15,23 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
-    console.log('rejected by auth');
-    // not logged in so redirect to login page with the return url
-    this.router.navigate(['/trade']);
-    return false;
+    return this.restfulCommService.authenticate().pipe(
+        first(),
+        delay(20000), 
+        map((serverReturn) => {
+          console.log('auth succeeded');
+          return true;
+        }),
+        catchError(err => {
+          console.log('auth failed');
+          return of(false);
+        })
+      )
+
+
+    // console.log('rejected by auth');
+    // // not logged in so redirect to login page with the return url
+    // this.router.navigate(['/trade']);
+    // return false;
   }
 }
